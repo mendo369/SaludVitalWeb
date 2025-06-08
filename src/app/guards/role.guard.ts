a2340e3c0efad12cfdbe176e4032965aa7ff919d
Luis Mendoza <luis23luis33@gmail.com>
2025-06-08 11:28:27 -0500

    roles guard

--- src/app/guards/role.guard.ts
+++ src/app/guards/role.guard.ts
@@ -1,15 +1,46 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { AuthService } from '../auth/services/auth.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoleGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+  ): boolean {
+    const requiredRole = route.data['requiredRole'];
+    const usuario = this.authService.getUsuario();
+
+    if (!usuario) {
+      this.router.navigate(['/']); // No está logueado
+      return false;
+    }
+
+    if (usuario.idRol === requiredRole) {
+      return true; // Tiene permiso
+    }
+
+    // Redirigir según su rol real
+    switch (usuario.idRol) {
+      case 1:
+        this.router.navigate(['/admin']);
+        break;
+      case 2:
+        this.router.navigate(['/doctor']);
+        break;
+      case 3:
+        this.router.navigate(['/recepcionista']);
+        break;
+      default:
+        this.router.navigate(['/']);
+    }
+
+    return false;
   }
-  
 }
