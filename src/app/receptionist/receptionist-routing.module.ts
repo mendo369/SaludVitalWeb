62ccf471113add9e4c599356f4fc57db4801a212
Luis Mendoza <luis23luis33@gmail.com>
2025-06-08 11:26:59 -0500

    Receptionist view

--- src/app/receptionist/receptionist-routing.module.ts
+++ src/app/receptionist/receptionist-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ReceptionistComponent } from './receptionist.component';
+import { DoctorsComponent } from './components/doctors/doctors.component';
+import { PatientsComponent } from './components/patients/patients.component';
+import { AppointmentComponent } from './components/appointment/appointment.component';
 
-const routes: Routes = [{ path: '', component: ReceptionistComponent }];
+const routes: Routes = [{
+    path: '',
+    component: ReceptionistComponent,
+    children: [
+      { path: 'doctores', component: DoctorsComponent },
+      { path: 'pacientes', component: PatientsComponent },
+      { path: 'citas', component: AppointmentComponent },
+      // Ruta por defecto (opcional)
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+    ]
+  }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/receptionist/receptionist.module.ts
+++ src/app/receptionist/receptionist.module.ts
@@ -3,13 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { ReceptionistRoutingModule } from './receptionist-routing.module';
 import { ReceptionistComponent } from './receptionist.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { PatientsComponent } from './components/patients/patients.component';
+import { AppointmentComponent } from './components/appointment/appointment.component';
+import { DoctorsComponent } from './components/doctors/doctors.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
     ReceptionistComponent,
-    DashboardComponent
+    PatientsComponent,
+    AppointmentComponent,
+    DoctorsComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule,
