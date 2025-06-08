f830c351b1be9b5a1661dae2e48692512a792787
Luis Mendoza <luis23luis33@gmail.com>
2025-06-08 11:28:53 -0500

    environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,20 @@
-export const environment = {};
+export const environment = {
+    backendUrl: 'http://localhost:8080/api/',
+    roles:[
+        {
+            value:1,
+            rol:'ADMIN',
+            route:'/admin'
+        },
+        {
+            value:2,
+            rol:'MEDICO',
+            route:'/doctor'
+        },
+        {
+            value:3,
+            rol:'RECEPCIONISTA',
+            route:'/recepcionist'
+        },
+    ]
+};
