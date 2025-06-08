8394fc28ea248c2015808527f9de5d417be3958e
Luis Mendoza <luis23luis33@gmail.com>
2025-06-08 11:29:15 -0500

    Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RoleGuard } from './guards/role.guard';
 
 const routes: Routes = [
   {
     path: 'admin',
-    loadChildren: () =>
-      import('./admin/admin.module').then((m) => m.AdminModule),
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
+    canActivate: [RoleGuard],
+    data: { requiredRole: 1 } // Solo Admin puede entrar
   },
   {
     path: 'doctor',
-    loadChildren: () =>
-      import('./doctor/doctor.module').then((m) => m.DoctorModule),
+    loadChildren: () => import('./doctor/doctor.module').then(m => m.DoctorModule),
+    canActivate: [RoleGuard],
+    data: { requiredRole: 2 } // Solo Doctor
   },
   {
-    path: 'receptionist',
-    loadChildren: () =>
-      import('./receptionist/receptionist.module').then(
-        (m) => m.ReceptionistModule
-      ),
+    path: 'recepcionista',
+    loadChildren: () => import('./receptionist/receptionist.module').then(m => m.ReceptionistModule),
+    canActivate: [RoleGuard],
+    data: { requiredRole: 3 } // Solo Recepcionista
   },
   {
     path: '',
-    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
